431b585184b0058d43ee190a5e5b93d2e4f994e6
maxteroit <adittyasubagja13@gmail.com>
2024-02-11 21:34:02 +0700

    set to default

--- src/core/api.js
+++ src/core/api.js
@@ -89,7 +89,7 @@ export function runAPI(express, app, gitCommit, gitBranch, __dirname) {
             let lang = languageCode(req);
             let j = apiJSON(0, { t: "bad request" });
             try {
-                // let contentCon = String(req.header('content-type')) === "application/json";
+                let contentCon = String(req.header('content-type')) === "application/json";
                 let request = req.body;
                 if (request.url) {
                     request.dubLang = request.dubLang ? lang : false;
